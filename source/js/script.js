1e6ec037689699336b22bbaf3c270d97b2eacb82
Stas <stas001w@mail.ru>
2021-04-24 19:59:21 +0300

    Header Nav

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+let navMain = document.querySelector(".main-navigation");
+let navToggle = document.querySelector(".main-navigation__toggle");
 
-pageHeader.classList.remove('page-header--nojs');
+navMain.classList.remove("main-navigation--nojs");
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains("main-navigation--closed")) {
+    navMain.classList.remove("main-navigation--closed");
+    navMain.classList.add("main-navigation--opened");
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    navMain.classList.add("main-navigation--closed");
+    navMain.classList.remove("main-navigation--opened");
   }
 });
+
